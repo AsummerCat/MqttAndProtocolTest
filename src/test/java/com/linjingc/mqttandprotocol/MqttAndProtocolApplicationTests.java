d152914dd0d3fef02022c8a3a9cbc373be0d60dd
chenxiaocheng <583188551@qq.com>
2022-10-10 11:06:11 +0800

    Initial commit

---
+++ src/test/java/com/linjingc/mqttandprotocol/MqttAndProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linjingc.mqttandprotocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttAndProtocolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
